ac0a27a966c79fa63dc5b5acca5e3129f294bf69
Ashwill Paulse <Ashwill.Paulse@dyna-mo.com>
2023-09-06 20:43:37 +0200

    create the worker dashboard

--- work_for_me/lib/worker/worker_dashboard.dart
+++ work_for_me/lib/worker/worker_dashboard.dart
@@ -9,8 +9,58 @@ class WorkerDashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Worker Dashboard'),
       ),
-      body: const Center(
-        child: Text('Welcome to the Worker Dashboard'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Star Rating Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.star, color: Colors.yellow),
+                Icon(Icons.star, color: Colors.yellow),
+                Icon(Icons.star, color: Colors.yellow),
+                Icon(Icons.star_border),
+                Icon(Icons.star_border),
+              ],
+            ),
+            const SizedBox(height: 32),
+            // Buttons Column
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Implement logic here
+                  },
+                  child: const Text('Logout'),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Implement logic here
+                  },
+                  child: const Text('My Profile'),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Implement logic here
+                  },
+                  child: const Text('View Orders'),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Implement logic here
+                  },
+                  child: const Text('Payments'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- work_for_me/windows/runner/flutter_window.cpp
+++ work_for_me/windows/runner/flutter_window.cpp
@@ -2,8 +2,6 @@
 
 #include <optional>
 
-#include "flutter/generated_plugin_registrant.h"
-
 FlutterWindow::FlutterWindow(const flutter::DartProject& project)
     : project_(project) {}
 
@@ -24,7 +22,7 @@ bool FlutterWindow::OnCreate() {
   if (!flutter_controller_->engine() || !flutter_controller_->view()) {
     return false;
   }
-  RegisterPlugins(flutter_controller_->engine());
+  //RegisterPlugins(flutter_controller_->engine());
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
 
   flutter_controller_->engine()->SetNextFrameCallback([&]() {
@@ -34,7 +32,7 @@ bool FlutterWindow::OnCreate() {
   // Flutter can complete the first frame before the "show window" callback is
   // registered. The following call ensures a frame is pending to ensure the
   // window is shown. It is a no-op if the first frame hasn't completed yet.
-  flutter_controller_->ForceRedraw();
+  //flutter_controller_->ForceRedraw();
 
   return true;
 }
