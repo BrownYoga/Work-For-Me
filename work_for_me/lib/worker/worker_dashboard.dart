b9e08366f84b3f4bb78aa8138150b7afd601ac51
Ashwill Paulse <Ashwill.Paulse@dyna-mo.com>
2023-09-06 20:53:07 +0200

    Update worker_dashboard.dart

--- work_for_me/lib/worker/worker_dashboard.dart
+++ work_for_me/lib/worker/worker_dashboard.dart
@@ -9,58 +9,72 @@ class WorkerDashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Worker Dashboard'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Star Rating Row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Buttons at the top
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
               children: [
-                Icon(Icons.star, color: Colors.yellow),
-                Icon(Icons.star, color: Colors.yellow),
-                Icon(Icons.star, color: Colors.yellow),
-                Icon(Icons.star_border),
-                Icon(Icons.star_border),
-              ],
-            ),
-            const SizedBox(height: 32),
-            // Buttons Column
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement logic here
-                  },
-                  child: const Text('Logout'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Navigate to My Profile
+                    },
+                    child: const Text('My Profile'),
+                  ),
                 ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement logic here
-                  },
-                  child: const Text('My Profile'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Navigate to View Orders
+                    },
+                    child: const Text('View Orders'),
+                  ),
                 ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement logic here
-                  },
-                  child: const Text('View Orders'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Navigate to Payments
+                    },
+                    child: const Text('Payments'),
+                  ),
                 ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    // Implement logic here
-                  },
-                  child: const Text('Payments'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.popUntil(context, ModalRoute.withName('/home'));
+                    },
+                    child: const Text('Logout'),
+                  ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          // Rating at the bottom
+          Column(
+            children: [
+              const Text('Your Rating',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(5, (index) {
+                  return const Icon(Icons.star, color: Colors.yellow);
+                  // Replace with logic to display half-stars if needed
+                }),
+              ),
+              const Text('Rated by 150 workers',
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
