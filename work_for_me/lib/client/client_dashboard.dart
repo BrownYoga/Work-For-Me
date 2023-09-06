2baef452721a675f643417b64a304bb6314cdd2a
Ashwill Paulse <Ashwill.Paulse@dyna-mo.com>
2023-09-06 21:17:40 +0200

    dsfdsf

--- work_for_me/lib/client/client_dashboard.dart
+++ work_for_me/lib/client/client_dashboard.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class ClientDashboard extends StatelessWidget {
+  const ClientDashboard({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Client Dashboard'),
+        title: const Text('Client Dashboard'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -14,38 +16,37 @@ class ClientDashboard extends StatelessWidget {
           // Your Buttons Here
           ElevatedButton(
             onPressed: () {
-              // TODO: Implement logout functionality
               Navigator.pushNamed(context, '/home');
             },
-            child: Text('Logout'),
+            child: const Text('Logout'),
           ),
           ElevatedButton(
             onPressed: () {
               // TODO: Implement active orders functionality
             },
-            child: Text('Active Orders'),
+            child: const Text('Active Orders'),
           ),
           ElevatedButton(
             onPressed: () {
               // TODO: Implement find a worker functionality
             },
-            child: Text('Find a Worker'),
+            child: const Text('Find a Worker'),
           ),
           ElevatedButton(
             onPressed: () {
               // TODO: Implement view outstanding payments functionality
             },
-            child: Text('View Outstanding Payments'),
+            child: const Text('View Outstanding Payments'),
           ),
           ElevatedButton(
             onPressed: () {
               // TODO: Implement profile functionality
             },
-            child: Text('Profile'),
+            child: const Text('Profile'),
           ),
 
           // Your Rating Bar
-          Text(
+          const Text(
             'Your Rating',
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
@@ -55,7 +56,7 @@ class ClientDashboard extends StatelessWidget {
             itemSize: 30,
             allowHalfRating: true,
             itemCount: 5,
-            itemBuilder: (context, _) => Icon(
+            itemBuilder: (context, _) => const Icon(
               Icons.star,
               color: Colors.amber,
             ),
@@ -63,7 +64,7 @@ class ClientDashboard extends StatelessWidget {
               print(rating);
             },
           ),
-          Text(
+          const Text(
             'Rated by 150 workers',
             style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
           ),
