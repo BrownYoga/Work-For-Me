80051d49b3ab1427f1a266d1932e0dc3e5b23c13
Ashwill Paulse <Ashwill.Paulse@dyna-mo.com>
2023-09-06 21:32:31 +0200

    Update client_dashboard.dart

--- work_for_me/lib/client/client_dashboard.dart
+++ work_for_me/lib/client/client_dashboard.dart
@@ -13,65 +13,73 @@ class ClientDashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Client Dashboard'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          // Your Buttons Here
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/home');
-            },
-            child: const Text('Logout'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // TODO: Implement active orders functionality
-            },
-            child: const Text('Active Orders'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // TODO: Implement find a worker functionality
-            },
-            child: const Text('Find a Worker'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // TODO: Implement view outstanding payments functionality
-            },
-            child: const Text('View Outstanding Payments'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // TODO: Implement profile functionality
-            },
-            child: const Text('Profile'),
-          ),
-
-          // Your Rating Bar
-          const Text(
-            'Your Rating',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          RatingBar.builder(
-            initialRating: 3.5,
-            minRating: 1,
-            itemSize: 30,
-            allowHalfRating: true,
-            itemCount: 5,
-            itemBuilder: (context, _) => const Icon(
-              Icons.star,
-              color: Colors.amber,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Reordered Buttons Here
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Implement profile functionality
+              },
+              child: const Text('Profile'),
             ),
-            onRatingUpdate: (rating) {
-              logger.i('Rating changed to: $rating');  // Using logger here
-            },
-          ),
-          const Text(
-            'Rated by 150 workers',
-            style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
-          ),
-        ],
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Implement find a worker functionality
+              },
+              child: const Text('Find a Worker'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Implement active orders functionality
+              },
+              child: const Text('Active Orders'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // TODO: Implement view outstanding payments functionality
+              },
+              child: const Text('View Outstanding Payments'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              child: const Text('Logout'),
+            ),
+
+            // Your Rating Bar
+            Column(
+              children: [
+                const Text(
+                  'Your Rating',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10), // Adjust the space
+                RatingBar.builder(
+                  initialRating: 3.5,
+                  minRating: 1,
+                  itemSize: 30,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemBuilder: (context, _) => const Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    logger.i('Rating changed to: $rating'); // Using logger here
+                  },
+                ),
+                const SizedBox(height: 10), // Adjust the space
+                const Text(
+                  'Rated by 150 workers',
+                  style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
