83b3e32e68fbe6b5d237c903a91a4fe109950315
Ashwill Paulse <Ashwill.Paulse@dyna-mo.com>
2023-09-13 22:11:52 +0200

    Update client_dashboard.dart

--- work_for_me/lib/client/views/client_dashboard.dart
+++ work_for_me/lib/client/views/client_dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:logger/logger.dart';
+import 'package:provider/provider.dart';
+import 'package:work_for_me/client/viewmodels/client_dashboard_viewmodel.dart'; // Make sure to import your ViewModel
 
 var logger = Logger();
 
@@ -9,76 +11,79 @@ class ClientDashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Client Dashboard'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            // Reordered Buttons Here
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/client_profile');
-              },
-              child: const Text('Profile'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/client_find_a_worker');
-              },
-              child: const Text('Find a Worker'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/client_active_orders');
-              },
-              child: const Text('Active Orders'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Implement view outstanding payments functionality
-              },
-              child: const Text('View Outstanding Payments'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/home');
-              },
-              child: const Text('Logout'),
-            ),
+    return ChangeNotifierProvider(
+      create: (context) => ClientDashboardViewModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Client Dashboard'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              // Reordered Buttons Here
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/client_profile');
+                },
+                child: const Text('Profile'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/client_find_a_worker');
+                },
+                child: const Text('Find a Worker'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/client_active_orders');
+                },
+                child: const Text('Active Orders'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement view outstanding payments functionality
+                },
+                child: const Text('View Outstanding Payments'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: const Text('Logout'),
+              ),
 
-            // Your Rating Bar
-            Column(
-              children: [
-                const Text(
-                  'Your Rating',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10), // Adjust the space
-                RatingBar.builder(
-                  initialRating: 3.5,
-                  minRating: 1,
-                  itemSize: 30,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemBuilder: (context, _) => const Icon(
-                    Icons.star,
-                    color: Colors.amber,
+              // Your Rating Bar
+              Column(
+                children: [
+                  const Text(
+                    'Your Rating',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  onRatingUpdate: (rating) {
-                    logger.i('Rating changed to: $rating'); // Using logger here
-                  },
-                ),
-                const SizedBox(height: 10), // Adjust the space
-                const Text(
-                  'Rated by 150 workers',
-                  style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(height: 10), // Adjust the space
+                  RatingBar.builder(
+                    initialRating: 3.5,
+                    minRating: 1,
+                    itemSize: 30,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemBuilder: (context, _) => const Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {
+                      logger.i('Rating changed to: $rating'); // Using logger here
+                    },
+                  ),
+                  const SizedBox(height: 10), // Adjust the space
+                  const Text(
+                    'Rated by 150 workers',
+                    style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
